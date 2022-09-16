2d64d1f3d32e569db368f404278ea690183414c4
HieuShinDepTrai <90301374+HieuShinDepTrai@users.noreply.github.com>
2022-09-17 01:45:20 +0700

    Add DBContext + User Model

---
+++ LearningProject/src/java/Models/User.java
@@ -0,0 +1,151 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package Models;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author Hieu Shin
+ */
+public class User {
+    private int UserID;
+    private String FirstName;
+    private String LastName;
+    private String Email;
+    private String PhoneNumber;
+    private String Country;
+    private String City;
+    private String Address;
+    private Date Dob;
+    private String PostCode;
+    private float Balance;
+    private byte[] Avatar;
+    private String Username;
+    private String Password;
+    private String Role;
+
+    public int getUserID() {
+        return UserID;
+    }
+
+    public void setUserID(int UserID) {
+        this.UserID = UserID;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String FirstName) {
+        this.FirstName = FirstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String LastName) {
+        this.LastName = LastName;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setPhoneNumber(String PhoneNumber) {
+        this.PhoneNumber = PhoneNumber;
+    }
+
+    public String getCountry() {
+        return Country;
+    }
+
+    public void setCountry(String Country) {
+        this.Country = Country;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String Address) {
+        this.Address = Address;
+    }
+
+    public Date getDob() {
+        return Dob;
+    }
+
+    public void setDob(Date Dob) {
+        this.Dob = Dob;
+    }
+
+    public String getPostCode() {
+        return PostCode;
+    }
+
+    public void setPostCode(String PostCode) {
+        this.PostCode = PostCode;
+    }
+
+    public float getBalance() {
+        return Balance;
+    }
+
+    public void setBalance(float Balance) {
+        this.Balance = Balance;
+    }
+
+    public byte[] getAvatar() {
+        return Avatar;
+    }
+
+    public void setAvatar(byte[] Avatar) {
+        this.Avatar = Avatar;
+    }
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public void setUsername(String Username) {
+        this.Username = Username;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+
+    public String getRole() {
+        return Role;
+    }
+
+    public void setRole(String Role) {
+        this.Role = Role;
+    }
+    
+}
