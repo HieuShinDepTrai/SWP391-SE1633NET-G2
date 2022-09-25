0d9f32cc85887ca6ffd33d7ab93d82191150b972
dungssj <dungldqhe161262@fpt.edu.vn>
2022-09-25 15:33:03 +0700

    update comment reply and post comment (front-end) for course watch

--- LearningProject/web/assets/js/course_watch.js
+++ LearningProject/web/assets/js/course_watch.js
@@ -1,21 +1,40 @@
-let course_section = document.querySelectorAll('.course-lesson-container');
-course_section.forEach(function(current) {
-    let course_lesson = current.children[1];
-    current.children[0].onclick = function() {
-        if(course_lesson.style.display === 'none') {
-            course_lesson.style.display = 'block';
-        } else {
-            course_lesson.style.display = 'none';
-        }
+let course_section = document.querySelectorAll(".course-lesson-container");
+course_section.forEach(function (current) {
+  let course_lesson = current.children[1];
+  current.children[0].onclick = function () {
+    if (course_lesson.style.display === "none") {
+      course_lesson.style.display = "block";
+    } else {
+      course_lesson.style.display = "none";
     }
-    // current.onclick = function() {
-    //     console.log(this);
-        // let course_lesson = this.children[1];
-        // console.log(course_lesson);
-        // if(course_lesson.style.display === 'none') {
-        //     course_lesson.style.display = 'block';
-        // } else {
-        //     course_lesson.style.display = 'none';
-        // }
-    // }
-})
+  };
+  // current.onclick = function() {
+  //     console.log(this);
+  // let course_lesson = this.children[1];
+  // console.log(course_lesson);
+  // if(course_lesson.style.display === 'none') {
+  //     course_lesson.style.display = 'block';
+  // } else {
+  //     course_lesson.style.display = 'none';
+  // }
+  // }
+});
+
+function auto_height(elem) {
+  /* javascript */
+  elem.style.height = "28px";
+  elem.style.height = elem.scrollHeight + "px";
+}
+
+function active_comment_button(current) {
+  let submit_comment = $(current).parent().next().children(".submit-comment");
+  if (current.value !== "") {
+    $(submit_comment).addClass("submit-comment-active");
+  } else {
+    $(submit_comment).removeClass("submit-comment-active");
+  }
+}
+
+function show_reply_post_comment(elem) {
+    $(elem).parent().next().toggleClass('d-none');
+}
