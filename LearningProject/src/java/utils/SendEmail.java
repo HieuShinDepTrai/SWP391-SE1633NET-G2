33d56980d5f0e49d58310eee0c6a0f1bf8cc3576
huongprowar <huongprowar@gmail.com>
2022-09-26 15:26:39 +0700

    fix home.jsp and add courseID to courseDao

--- LearningProject/src/java/utils/SendEmail.java
+++ LearningProject/src/java/utils/SendEmail.java
@@ -47,7 +47,7 @@ public class SendEmail {
             msg.addHeader("Content-Transfer-Encoding", "8bit");
             msg.setFrom(new InternetAddress(fromEmail, "NoReply-JD"));
             msg.setReplyTo(InternetAddress.parse(fromEmail, false));
-            msg.setSubject(subject, "UTF-8");
+            msg.setSubject(subject, "UTF-8");            
             msg.setText(body, "UTF-8");
             msg.setSentDate(new Date());
             msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail, false));
