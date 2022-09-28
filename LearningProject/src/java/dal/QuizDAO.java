dbe6cfff554bf37da34712411ff785d72b999d80
hungdeptrai1311 <vumanhhung.bc2017@gmail.com>
2022-09-28 22:16:17 +0700

    create dao class and method for course work

---
+++ LearningProject/src/java/dal/QuizDAO.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+import Model.Quiz;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author vuman
+ */
+public class QuizDAO extends DBContext{
+    public Quiz getQuizOfLesson(int lessonId){
+        try(ResultSet rs = executeQuery("SELECT [QuizID], [Mark] FROM [dbo].[Quiz] WHERE [LessonID] = ?", lessonId)){
+            return new Quiz(rs.getInt("QuizID"), rs.getDouble("Mark"), lessonId);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
