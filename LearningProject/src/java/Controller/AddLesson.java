686e5824811f04ef658b87b062020875ecf3840d
HieuShinDepTrai <90301374+HieuShinDepTrai@users.noreply.github.com>
2022-09-30 01:41:17 +0700

    done: update avatar + resize image

--- LearningProject/src/java/Controller/AddLesson.java
+++ LearningProject/src/java/Controller/AddLesson.java
@@ -5,6 +5,7 @@
 
 package Controller;
 
+import Model.Lesson;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
@@ -27,19 +28,6 @@ public class AddLesson extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet AddLesson</title>");  
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet AddLesson at " + request.getContextPath () + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
     } 
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -53,7 +41,13 @@ public class AddLesson extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        processRequest(request, response);
+        String title = request.getParameter("lesson_tilte");
+        String content = request.getParameter("lesson_content");
+        String type = request.getParameter("type");
+        
+        if(type.compareTo("Video") == 0) {
+            
+        }
     } 
 
     /** 
--- LearningProject/web/assets/js/account_profile.js
+++ LearningProject/web/assets/js/account_profile.js
@@ -3,56 +3,53 @@ addEventListener("load", () => {
     var course = document.querySelector('.link-course');
     var menu = document.querySelectorAll('.menu');
 
-    function resetMenu() {
-        menu.forEach((item) => {
-            item.classList.remove('menu-active');
-        })
-    }
-
-
-    function reset() {
-        profile.classList.remove('active');
-        course.classList.remove('active');
-    }
-
-    function profile_link(menu) {
-        reset();
-        resetMenu();
-        profile.classList.add('active');
-        menu.classList.add('menu-active');
-    }
-
-    function course_link(menu) {
-        reset();
-        resetMenu();
-        course.classList.add('active');
-        menu.classList.add('menu-active');
-    }
-
-
     var avatar_preview = document.querySelector('.avatar-preview img');
     var formFile = document.querySelector('.modal-body #formFile');
+
+    /* Disable input form */
+    
 
     formFile.addEventListener('change', (e) => {
         if (e.target.files.length) {
             const src = URL.createObjectURL(e.target.files[0]);
             avatar_preview.src = src;
         }
-    })
-
-    /* Disable input form */
-    var isDisaled = true;
-    var saveBtn = document.querySelector('#save');
-    function disableOff(button) {
-        var input = document.querySelectorAll('.account-information-section input');
-        input.forEach((item) => {
-            item.disabled = false;
-        })
-        button.classList.add('d-none');
-        saveBtn.classList.remove('d-none');
-        isDisaled = false;
-    }
+    });
 });
+    
+function resetMenu() {
+    menu.forEach((item) => {
+        item.classList.remove('menu-active');
+    });
+}
+
+function reset() {
+    profile.classList.remove('active');
+    course.classList.remove('active');
+}
+
+function profile_link(menu) {
+    reset();
+    resetMenu();
+    profile.classList.add('active');
+    menu.classList.add('menu-active');
+}
+
+function course_link(menu) {
+    reset();
+    resetMenu();
+    course.classList.add('active');
+    menu.classList.add('menu-active');
+}
+
+function disableOff(button) {
+    var input = document.querySelectorAll('.account-information-section input');
+    input.forEach((item) => {
+        item.disabled = false;
+    })
+    button.classList.add('d-none');
+    document.querySelector('#save').classList.remove('d-none');
+}
 
 function UploadAvatar(target) {
     var reader = new FileReader();
