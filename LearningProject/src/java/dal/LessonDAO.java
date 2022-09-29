e8658e3c29cf29133f2c2a7bd77d84fd7e7c1815
dungssj <dungldqhe161262@fpt.edu.vn>
2022-09-29 20:03:53 +0700

    Create Course Detail Controller

--- LearningProject/src/java/dal/LessonDAO.java
+++ LearningProject/src/java/dal/LessonDAO.java
@@ -23,9 +23,9 @@ public class LessonDAO extends DBContext{
     
     public ArrayList<Lesson> getAllLessonOfSection(int sectionId){
         ArrayList<Lesson> lessonlist = new ArrayList<Lesson>();
-        try(ResultSet rs = executeQuery("SELECT [LessonID], [LessonName], [isDisable], [type] FROM [dbo].[Lesson] WHERE [SectionID] = ? AND [isDisable] = 0", sectionId)){
+        try(ResultSet rs = executeQuery("SELECT [LessonID], [LessonName], [isDisable], [type], [isChecked] FROM [dbo].[Lesson] WHERE [SectionID] = ? AND [isDisable] = 0", sectionId)){
             while(rs.next()){
-                lessonlist.add(new Lesson(rs.getInt("LessonID"), sectionId, rs.getNString("LessonName"), rs.getByte("isDisable"), rs.getString("types")));
+                lessonlist.add(new Lesson(rs.getInt("LessonID"), sectionId, rs.getNString("LessonName"), rs.getByte("isDisable"), rs.getString("types"), rs.getBoolean("isChecked")));
             }
         }
         catch(Exception e){
