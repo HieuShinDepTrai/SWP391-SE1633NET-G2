90e263e8b1f56974ed462e3471560d21431a6808
dungssj <dungldqhe161262@fpt.edu.vn>
2022-09-30 22:25:04 +0700

    add show section list and lesson list for course watch page

--- LearningProject/src/java/Controller/CourseWatchController.java
+++ LearningProject/src/java/Controller/CourseWatchController.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package Controller;
 
 import Model.Course;
@@ -24,34 +23,37 @@ import java.util.ArrayList;
  * @author Dung
  */
 public class CourseWatchController extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
+        try ( PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet CourseWatchController</title>");  
+            out.println("<title>Servlet CourseWatchController</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet CourseWatchController at " + request.getContextPath () + "</h1>");
+            out.println("<h1>Servlet CourseWatchController at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -59,35 +61,45 @@ public class CourseWatchController extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         // Check if user login or not
-        if(request.getSession().getAttribute("user") != null) {
+//        if (request.getSession().getAttribute("user") != null) {
             // Get course id 
             int id = Integer.parseInt(request.getParameter("id"));
-            
+
             CourseDAO cdao = new CourseDAO();
             SectionDAO sdao = new SectionDAO();
             LessonDAO ldao = new LessonDAO();
-            
+
             // Get data from dao
             Course c = cdao.getCourseInformation(id);
             ArrayList<Section> listSection = sdao.getAllSectionOfCourse(id);
-            ArrayList<Lesson> listLesson = ldao.getAllLessonOfSection(id);
-            
+            ArrayList<Lesson> listLesson = new ArrayList<>();
+            for (Section section : listSection) {
+                ArrayList<Lesson> tmp = ldao.getAllLessonOfSection(section.getSectionId());
+                for (Lesson lesson : tmp) {
+                    int lessonSecond = lesson.getTime();
+                    int Minute = lessonSecond / 60;
+                    int Second = lessonSecond % 60;
+                    listLesson.add(lesson);
+                }
+            }
+
             // Send to jsp
             request.setAttribute("course", c);
             request.setAttribute("listSection", listSection);
             request.setAttribute("listLesson", listLesson);
-            
-            request.getRequestDispatcher("CourseWatch.jsp").forward(request, response);
-        } else {
-            // Send back to home pages
-            response.sendRedirect("home");
-        }
-    } 
 
-    /** 
+            request.getRequestDispatcher("CourseWatch.jsp").forward(request, response);
+//        } else {
+//            // Send back to home pages
+//            response.sendRedirect("home");
+//        }
+    }
+
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -95,12 +107,13 @@ public class CourseWatchController extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
