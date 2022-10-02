7c335f1f0005a2b7ed29248bcd673d80d95bbaeb
HieuShinDepTrai <90301374+HieuShinDepTrai@users.noreply.github.com>
2022-10-02 23:55:35 +0700

    fix addlesson.java

--- LearningProject/src/java/Controller/AddLesson.java
+++ LearningProject/src/java/Controller/AddLesson.java
@@ -9,11 +9,11 @@ import Model.Section;
 import dal.LessonDAO;
 import dal.SectionDAO;
 import java.io.IOException;
-import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import java.util.ArrayList;
 
 /**
