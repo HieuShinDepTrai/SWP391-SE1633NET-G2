9b759f43bf37975a77676804af00fce88e03518a
dungssj <dungldqhe161262@fpt.edu.vn>
2022-10-13 19:58:29 +0700

    add get question and answer function

--- LearningProject/src/java/dal/QuestionDAO.java
+++ LearningProject/src/java/dal/QuestionDAO.java
@@ -4,6 +4,7 @@
  */
 package dal;
 
+import Model.Answer;
 import Model.Question;
 import java.sql.ResultSet;
 import java.util.ArrayList;
@@ -12,18 +13,42 @@ import java.util.ArrayList;
  *
  * @author vuman
  */
-public class QuestionDAO extends DBContext{
-    public ArrayList<Question> getQuestionsOfQuiz(int quizId){
+public class QuestionDAO extends DBContext {
+
+    public ArrayList<Question> getQuestionsOfQuiz(int quizId) {
         ArrayList<Question> questionlist = new ArrayList<Question>();
-        try(ResultSet rs = executeQuery("SELECT [QuestionID], [QuestionContent] FROM [dbo].[Question] WHERE [QuizID] = ?", quizId)){
-            while(rs.next()){
+        try ( ResultSet rs = executeQuery("SELECT [QuestionID], [QuestionContent] FROM [dbo].[Question] WHERE [QuizID] = ?", quizId)) {
+            while (rs.next()) {
                 questionlist.add(new Question(rs.getInt("QuestionID"), rs.getNString("QuestionContent"), quizId));
             }
             return questionlist;
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public Question getQuestionAndAnswer(int questionID) {
+        Question q = new Question();
+        try {
+            ResultSet rs = executeQuery("select q.QuestionID, q.QuestionContent,a.AnswerContent, a.isCorrect from dbo.Question q\n"
+                    + "join Answer a on q.QuestionID = a.QuestionID\n"
+                    + "where q.QuestionID = ?", questionID);
+            ArrayList<Answer> answers = new ArrayList<>();
+            boolean status = false;
+            while(rs.next()) {
+                if(status == false) {
+                    q.setQuestionId(rs.getInt("QuestionID"));
+                    q.setQuestionContent(rs.getString("QuestionContent"));
+                    status = true;
+                }
+                answers.add(new Answer(0, rs.getString("AnswerContent"), 0, rs.getBoolean("isCorrect")));
+            }
+            q.setAnswers(answers);
+        } catch (Exception e) {
+            System.out.println("getQuestionAndAnswer: ");
+            e.printStackTrace();
+        }
+        return q;
     }
 }
