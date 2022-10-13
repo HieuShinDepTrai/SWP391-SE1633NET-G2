cbab99f9a30ec718f10a7679d6384d0774767790
huongprowar <huongprowar@gmail.com>
2022-10-13 22:02:36 +0700

    fix add duplicate gg account

--- LearningProject/src/java/Controller/GoogleLoginController/GoogleLoginController.java
+++ LearningProject/src/java/Controller/GoogleLoginController/GoogleLoginController.java
@@ -51,8 +51,7 @@ public class GoogleLoginController extends HttpServlet {
                 try {
                     Date date = new Date(serialVersionUID);
                     User user = new User(ggUser.getGiven_name(), ggUser.getFamily_name(), ggUser.getEmail(), "", "", "", "", date, "", 0, ggUser.getPicture(), ggUser.getId(), SHA256.SHA256(ggUser.getId()), "User", "", "", false);
-                    userDAO.addGoogleUser(user);
-                    userDAO.addUser(user);
+                    userDAO.addGoogleUser(user);                    
                     HttpSession session = request.getSession();
                     session.setAttribute("username", user.getUserName());
                     session.setAttribute("user", userDAO.getAllUserInformation(user.getUserName()));
@@ -65,7 +64,7 @@ public class GoogleLoginController extends HttpServlet {
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException {        
         doGet(request, response);
     }
 
