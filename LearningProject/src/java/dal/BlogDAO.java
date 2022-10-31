93aa0a6f76373f0305958d5bcc14c8790bf356e0
dungssj <dungldqhe161262@fpt.edu.vn>
2022-10-31 12:07:16 +0700

    Revert to 5657b0f44f29412f387de40f62343ba5980eb856

--- LearningProject/src/java/dal/BlogDAO.java
+++ LearningProject/src/java/dal/BlogDAO.java
@@ -54,7 +54,7 @@ public class BlogDAO extends DBContext {
 
     public Blog getBlogInformation(int blogId) {
         try {
-            ResultSet rs = executeQuery("SELECT [UserID], [BlogDate], [BlogContent], [BlogTitle], [BlogDescription], [BlogImage], [Category], [Status] "
+            ResultSet rs = executeQuery("SELECT [UserID], [BlogDate], [BlogContent], [BlogTilte], [BlogDescription], [BlogImage], [Category], [Status] "
                     + "FROM [dbo].[Blog] WHERE [BlogID] = ?", blogId);
 
             if (rs.next()) {
@@ -62,7 +62,7 @@ public class BlogDAO extends DBContext {
                         rs.getInt("UserID"),
                         rs.getTimestamp("BlogDate"),
                         rs.getNString("BlogContent"),
-                        rs.getNString("BlogTitle"),
+                        rs.getNString("BlogTilte"),
                         rs.getNString("BlogDescription"),
                         rs.getString("BlogImage"),
                         rs.getString("Category"),
